198d987463291152964bd83ceef31603b71a378b
Saurabh <saurabhv.pandey5@gmail.com>
2023-03-05 03:53:46 +0530

    first commit

---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectData } from "../pages/homeSlice";
+import styled from "styled-components";
+// Data
+import { Blog } from "../data";
+// Icons
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+const StyledSocialLinks = styled.div`
+  a {
+    margin: 0 1rem;
+  }
+`;
+
+export default function SocialLinks() {
+  const { blog, html_url } = useSelector(selectData);
+
+  return (
+    <StyledSocialLinks>
+      <a
+        href={html_url}
+        aria-label="Check out my GitHub profile."
+        className="link-icons"
+      >
+        <FaGithub />
+      </a>
+      {blog && (
+        <a href={blog} aria-label="External link" className="link-icons">
+          {Blog}
+        </a>
+        
+      )}
+      <a
+        href="https://www.linkedin.com/in/saurabh-pandey-367ab4193/"
+        aria-label="Check out my Linkedin profile."
+        className="link-icons"
+      >
+        <FaLinkedin />
+      </a>
+    </StyledSocialLinks>
+  );
+}
