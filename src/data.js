ece2004a3a5514899b1c07f40bd6fbca32164fac
Saurabh-Pandey79 <81317644+Saurabh-Pandey79@users.noreply.github.com>
2023-04-19 17:27:00 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -89,7 +89,7 @@ export const skillData = [
 ];
 
 // Resume link (string - "https://YourResumeUrl") - I am using CloudFront to share my resume (https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/Introduction.html)
-export const resume = "https://drive.google.com/file/d/1AVJpQKsjbrXysdluzhKwpzEOid-u2uQd/view?usp=sharing";
+export const resume = "https://drive.google.com/file/d/1AVJpQKsjbrXysdluzhKwpzEOid-u2uQd/view";
 
 /* Projects
  ************************************************************** 
